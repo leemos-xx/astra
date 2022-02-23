ab2a81f87a4f97da4e1faa8b04550b564cb18b9c
lih70 <lih70@chinatelecom.cn>
2022-02-23 21:41:32 +0800

    生命周期的阶段声明

---
+++ src/main/java/leemos/astra/Lifecycle.java
@@ -0,0 +1,45 @@
+package leemos.astra;
+
+import leemos.astra.exception.LifecycleException;
+
+/**
+ * 定义生命周期的各阶段
+ *
+ * @author lihao
+ * @date 2022年2月23日
+ * @version 1.0
+ */
+public interface Lifecycle {
+
+    /**
+     * 初始化阶段
+     *
+     * @throws LifecycleException
+     */
+    default void init() throws LifecycleException {
+    }
+
+    /**
+     * 启动阶段
+     *
+     * @throws LifecycleException
+     */
+    default void start() throws LifecycleException {
+    }
+
+    /**
+     * 停止阶段
+     *
+     * @throws LifecycleException
+     */
+    default void stop() throws LifecycleException {
+    }
+
+    /**
+     * 销毁阶段
+     *
+     * @throws LifecycleException
+     */
+    default void destroy() throws LifecycleException {
+    }
+}
