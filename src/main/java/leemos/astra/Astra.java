5c43a83c3588066effb904b3e2509333fc9579ce
lih70 <lih70@chinatelecom.cn>
2022-02-23 20:28:24 +0800

    Astra初始提交

---
+++ src/main/java/leemos/astra/Astra.java
@@ -0,0 +1,29 @@
+package leemos.astra;
+
+/**
+ * Astra
+ *
+ * @author lihao
+ * @date 2022年2月23日
+ * @version 1.0
+ */
+public class Astra {
+
+    private NodeConfig config;
+
+    public Astra(NodeConfig config) {
+        this.config = config;
+    }
+
+    public void start() {
+
+    }
+
+    public void stop() {
+
+    }
+
+    public NodeConfig getConfig() {
+        return config;
+    }
+}
